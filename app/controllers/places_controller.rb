9a73737af4719630cbc9c320856cab6ef75004d8
JoshuaDavid <jshuadvd@icloud.com>
2015-04-08 18:26:27 -0700

    Fixed Controller messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,19 +23,39 @@ class PlacesController < ApplicationController
 
   def create
     @place = current_user.places.new(place_params)
-    @place.save
-    respond_with(@place)
+
+    respond_to do |format|
+      if @place.save
+        format.html { redirect_to @place, notice: 'Place was successfully created.' }
+        format.json { render :show, status: :created, location: @place }
+      else
+        format.html { render :new }
+        format.json { render json: @place.errors, status: :unprocessable_entity }  
+      end
+    end
   end
 
   def update
-    @place.update(place_params)
-    respond_with(@place)
+
+    respond_to do |format|
+      if @place.update(place_params)
+        format.html { redirect_to @place, notice: 'Place was successfully updated.' }
+        format.json { render :show, status: :ok, location: @place }
+      else
+        format.html { render :edit }
+        format.json { render json: @place.errors, status: :unprocessable_entity }  
+      end
+    end
   end
 
   def destroy
     @place.destroy
-    respond_with(@place)
+    respond_to do |format|
+        format.html { redirect_to places_url, notice: 'Place was successfully destroyed.' }
+        format.json { head :no_content }
+    end  
   end
+
 
   private
     def set_place
